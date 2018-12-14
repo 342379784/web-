9c90e81dd683cc8720479dfd543049a0a9c08182
朱英俊 <30980874+342379784@users.noreply.github.com>
2018-12-14 15:34:18 +0800

    Update uploadFile.js

--- static/uploadFile.js
+++ static/uploadFile.js
@@ -4,90 +4,13 @@ $(function () {
 });
 
 function aaa() {
-    var fileMd5;
     var $list=$("#thelist");
     var state = 'pending';//初始按钮状态
-
-
-    //
-    // //监听分块上传过程中的三个时间点
-    // WebUploader.Uploader.register({
-    //     "before-send-file" : "beforeSendFile",
-    //     "before-send" : "beforeSend",
-    //     "after-send-file" : "afterSendFile",
-    // }, {
-    //     //时间点1：所有分块进行上传之前调用此函数
-    //     beforeSendFile : function(file) {
-    //         var deferred = WebUploader.Deferred();
-    //         //1、计算文件的唯一标记，用于断点续传
-    //         (new WebUploader.Uploader()).md5File(file, 0, 100 * 1024)
-    //             .progress(function(percentage) {
-    //                 $('#' + file.id).find("p.state").text("正在读取文件信息...");
-    //             }).then(function(val) {
-    //             fileMd5 = val;
-    //             $('#' + file.id).find("p.state").text("成功获取文件信息...");
-    //             //获取文件信息后进入下一步
-    //             deferred.resolve();
-    //         });
-    //         return deferred.promise();
-    //     },
-    //     //时间点2：如果有分块上传，则每个分块上传之前调用此函数
-    //     beforeSend : function(block) {
-    //         var deferred = WebUploader.Deferred();
-    //
-    //         $.ajax({
-    //             type : "POST",
-    //             url : "/ccc?action=checkChunk",
-    //             data : {
-    //                 //文件唯一标记
-    //                 fileMd5 : fileMd5,
-    //                 //当前分块下标
-    //                 chunk : block.chunk,
-    //                 //当前分块大小
-    //                 chunkSize : block.end - block.start,
-    //                 enctype : "multipart/form-data"
-    //             },
-    //             dataType : "json",
-    //             success : function(response) {
-    //                 if (response.ifExist) {
-    //                     //分块存在，跳过
-    //                     deferred.reject();
-    //                 } else {
-    //                     //分块不存在或不完整，重新发送该分块内容
-    //                     deferred.resolve();
-    //                 }
-    //             }
-    //         });
-    //
-    //         this.owner.options.formData.fileMd5 = fileMd5;
-    //         deferred.resolve();
-    //         return deferred.promise();
-    //     },
-    //     //时间点3：所有分块上传成功后调用此函数
-    //     afterSendFile : function() {
-    //         //如果分块上传成功，则通知后台合并分块
-    //         $.ajax({
-    //             type : "POST",
-    //             url : "/ccc?action=mergeChunks",
-    //             data : {
-    //                 fileMd5 : fileMd5,
-    //             },
-    //             success : function(response) {
-    //                 alert("上传成功");
-    //             }
-    //         });
-    //     }
-    // });
-
-
-
-
-
 
     uploader = WebUploader.create({
         swf: 'webuploader/Uploader.swf',// swf文件路径
         // fileVal: 'multiFile',  //提交到到后台，是要用"multiFile"这个名称属性来取文件的
-        server: '/bbb',// 文件接收服务端。
+        server: '/bbb',// 文件接收服务端。这里可以换成ccc来测试，当所有分片文件传送完成后触发的请求地址跟这里对应
         // 选择文件的按钮。可选。
         // 内部根据当前运行是创建，可能是input元素，也可能是flash.
         pick: '#picker',
